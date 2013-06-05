c1b61c7b2bfd0037b55ea422ead55ff7bf7e78d5
fiji-flo <flome.rz@gmail.com>
2013-06-06 00:59:54 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 duell
 =====
+
+Family Feud (german: Familien Duell)
+http://en.wikipedia.org/wiki/Family_Feud
+http://de.wikipedia.org/wiki/Familien-Duell
--- sd.py
+++ sd.py
@@ -1,40 +1,111 @@
 # -*- coding: utf-8 -*-
 import pygame
-import socket
 import time
+import BaseHTTPServer
+import select
+import urllib
+import errno
+
 from multiprocessing import Process, Queue
 
 
-def recv(q, r):
-    host = ''
-    port = 50000
-    backlog = 5
-    size = 1024 
+def srvr(q, r, x):
+    site = open("html.html").read()
+    jq = open("jquery-2.0.2.min.js").read()
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host,port))
-    s.listen(backlog) 
-    while r.empty():
-        client, address = s.accept()
-        data = client.recv(size)
-        q.put(data)
-        client.close()
+    class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+        def do_HEAD(s):
+            s.send_response(200)
+            s.send_header("Content-type", "text/html")
+            s.end_headers()
+
+        def do_GET(s):
+            """Respond to a GET request."""
+            print(s.headers)
+            #r, msg = s.deal_get()
+            #print(str(r)+": "+msg)
+            
+            s.send_response(200)
+            if "jquery-2.0.2.min.js" in s.path:
+                s.send_header("Content-type", "text/javascript")
+                s.end_headers()
+                s.wfile.write(bytes(jq))
+                return
+            elif "sddo" in s.path:
+                s.send_header("Content-type", "text")
+                s.end_headers()
+                q.put(urllib.unquote(s.path.split("?")[1]))
+                data = r.get(True,2 )
+                s.wfile.write(bytes(data))
+                return
+            s.send_header("Content-type", "text/html")
+            s.end_headers()
+            s.gen_site()
+
+        def finish(s):
+            pass
+
+        def gen_site(s):
+            s.wfile.write(bytes(site))
 
 
+    def _eintr_retry(func, *args):
+        """restart a system call interrupted by EINTR"""
+        while True:
+            try:
+                return func(*args)
+            except (OSError, select.error) as e:
+                if e.args[0] != errno.EINTR:
+                    raise
+
+    def run_server(httpd):
+        running = True
+        try:
+            while running:
+                if not x.empty():
+                    running = False
+                rl, w, e = _eintr_retry(select.select, [httpd], [], [], 0.5)
+                if httpd in rl:
+                    httpd._handle_request_noblock()
+        finally:
+            pass
+
+
+        
+
+    def start_server():
+        server_class = BaseHTTPServer.HTTPServer
+        httpd = server_class(("127.0.0.1", 8001), MyHandler)
+        print(time.asctime(), "Server Starts - %s:%s" % ("127.0.0.1", 8001))
+        try:
+            run_server(httpd)
+        except KeyboardInterrupt:
+            httpd.server_close()
+            print(time.asctime(), "Server Stops - %s:%s" % ("127.0.0.1", 8001))
+            return    
+        httpd.server_close()
+        print(time.asctime(), "Server Stops - %s:%s" % ("127.0.0.1", 8001))
+
+
+    start_server()
 
 class Team(object):
     def __init__(self, name, points=0):
         self.name = name
         self.points = points
 
-
-class Round(object):
+class Question(object):
     def __init__(self, r):
         l = r.split('\n')
-        a = [line.split(";") for line in l[2:]]
-        a = [(q, int(p)) for (q,p) in a]
-        self.question = r[1].strip()
+        a = [line.split(";") for line in l[2:] if line != ""]
+        a = [(qp[0], int(qp[1])) for qp in a if len(qp) == 2]
+        self.question = l[1].strip()
         self.answers = a
+
+class Round(Question):
+    def __init__(self, r):
+        super(Round, self).__init__(r)
         self.num = int(r[0])
         self.mult = 1 if self.num == 1 else self.num - 1
         self.num_answers = min(len(self.answers), 7 - self.num)
@@ -42,19 +113,71 @@ class Round(object):
         self.points = 0
         self.solved = []
 
+    def get_data(self):
+        ret = "{}###{}".format(self.question, "##".join(["{}#{}".format(a,p) for (a,p) in self.answers]))
+        return ret
+
+class Final(object):
+    def __init__(self, f):
+        self.questions = []
+        self.points_a = 0
+        self.points_b = 0
+        self.hide = False
+        self.solved_a = {}
+        self.solved_b = {}
+        fqs = f.strip().split("#fin")
+        for fq in fqs[1:]:
+            q = Question(fq.strip())
+            self.questions.append(q)
+
+    def get_data(self):
+        qs = []
+        for q in self.questions:
+            qs.append("{}###{}".format(q.question, "##".join(["{}#{}".format(a,p) for (a,p) in q.answers])))
+        return "####".join(qs)
+
 class Game(object):
     def __init__(self):
         self.team_a = None
         self.team_b = None
         self._rounds = {}
         self._cur_round = None
+        self.final = None
         self.rtype = None
+        self.solving = False
+        self.solving_a = False
+        self.solving_b = False
+        self.solving_num = None
+        self.solving_state = 0
+        self.solving_start = 0
+        self.solved = []
+        self.solved_fin_a = {}
+        self.solved_fin_b = {}
+        self.wrongs = 0
 
     def add_round(self, r):
-        self._rounds[r.num] = r
+        self._rounds[r.num-1] = r
 
     def set_round(self, n):
+        self.rtype = "round"
+        self.solving = False
+        self.solving_num = None
+        self.solving_state = 0
+        self.solving_start = 0
+        self.solved = []
+        self.wrongs = 0
         self._cur_round = self._rounds[n]
+
+    def set_final(self):
+        self.rtype = "final"
+        self.solving_a = False
+        self.solving_b = False
+        self.solving_num = None
+        self.solving_state = 0
+        self.solving_start = 0
+        self.solved_fin_a = {}
+        self.solved_fin_b = {}
+        self.wrongs = 0
 
     def set_team_a(self, name, points=0):
         self.team_a = Team(name, points) 
@@ -72,6 +195,13 @@ class Game(object):
         else:
             return 3
 
+    def make_points(self, team):
+        p = team.points
+        for i in self.solved:
+            p += (self._cur_round.answers[i][1]*self._cur_round.mult)
+        team.points = p
+
+
     def get_mult(self):
         if self._cur_round:
             return self._cur_round.mult
@@ -88,9 +218,13 @@ class Game(object):
         for rnd in rnds[1:]:
             r = Round(rnd.strip())
             self.add_round(r)
+        self.final = Final(f.strip())
                 
     def solve(self, num):
         self._cur_round.solved.append(num)
+
+    def wrong(self):
+        self._cur_round.wrongs = min(self._cur_round.wrongs, 3)
 
 
 
@@ -110,20 +244,16 @@ class Renderer(object):
         self._done=False
         self._clock=pygame.time.Clock()
         self._big_font_size = self._height // 12
-        self._small_font_size = self._height // 14
+        self._small_font_size = self._height // 16
         self._big_font = pygame.font.SysFont("DejaVu Sans Mono", self._big_font_size)
         self._small_font = pygame.font.SysFont("DejaVu Sans Mono", self._small_font_size)
         self._frame_count = 0
         self._frame_rate = 60
         self._title = "Studierenden Duell 2013"
-        self._solving = False
-        self._solving_num = None
-        self._solving_state = 0
-        self._solving_start = 0
-        self._solved = []
         self._alen = 20
         self._sound_answer = pygame.mixer.Sound("answer.wav")
         self._sound_points = pygame.mixer.Sound("points.wav")
+        self._sound_wrong = pygame.mixer.Sound("wrong.wav")
 
     def render(self, game=None):
         for event in pygame.event.get(): # User did something
@@ -147,69 +277,93 @@ class Renderer(object):
             self.draw_idle()
         elif game.rtype == "round":
             self.draw_round(game)
-        else:
+        elif game.rtype == "final":
             self.draw_final(game)
+        else:
+            self.draw_idle()
 
     def draw_round(self, game):
         rnd = game.get_round()
         for i in range(rnd.num_answers):
             a, p = rnd.answers[i]
-            if i in game.get_round().solved and not i in self._solved:
-                self._solving = True
-                self._solving_num = i
-                self._solving_state = 1
-                self._solving_start = self._frame_count
-                self._solved.append(i)
+            if i in game.get_round().solved and not i in game.solved:
+                game.solving = True
+                game.solving_num = i
+                game.solving_state = 1
+                game.solving_start = self._frame_count
+                game.solved.append(i)
                 self._sound_answer.play()
-            if self._solving and self._solving_num == i:
-                a,p = self.solve_answer(i, game)
-            elif not i in self._solved:
+            if game.solving and game.solving_num == i:
+                a,p = self.solve_answer(i, game, a, p)
+            elif not i in game.solved:
                 a = "."*self._alen
                 p = "--"
             self.draw_answer(i, a, p) 
         self.draw_points(game)
 
     def draw_points(self, game):
-        last_y = self._height - self._big_font.get_linesize()
-        snd_last_y = last_y - self._big_font.get_linesize()
+        last_y = self._height - self._small_font.get_linesize()
+        snd_last_y = last_y - self._small_font.get_linesize()
         # team a
         output_string = '{}'.format(game.team_a.name)
-        text = self._big_font.render(output_string.encode("utf-8"),True,self._green)
+        text = self._small_font.render(output_string.encode("utf-8"),True,self._green)
         self._screen.blit(text, [0, snd_last_y])
         output_string = '{}'.format(game.team_a.points)
-        text = self._big_font.render(output_string,True,self._green)
+        text = self._small_font.render(output_string,True,self._green)
         self._screen.blit(text, [0, last_y])
 
         #team_b
         output_string = '{}'.format(game.team_b.name)
-        text = self._big_font.render(output_string.encode("utf-8"),True,self._green)
+        text = self._small_font.render(output_string.encode("utf-8"),True,self._green)
         self._screen.blit(text, [self._width - text.get_size()[0], snd_last_y])
         output_string = '{}'.format(game.team_b.points)
-        text = self._big_font.render(output_string,True,self._green)
+        text = self._small_font.render(output_string,True,self._green)
         self._screen.blit(text, [self._width - text.get_size()[0], last_y])
 
         #wrong and multi
+        if game.wrongs < game.get_wrongs():
+            game.wrongs += 1
+            self._sound_wrong.play()
         output_string = '{} {:<3}'.format(game.get_round().points, "X"*game.get_wrongs())
-        text = self._big_font.render(output_string,True,self._green)
+        text = self._small_font.render(output_string,True,self._green)
         self._screen.blit(text, [self.center(text), snd_last_y])
         output_string = '{}x'.format(game.get_mult())
-        text = self._big_font.render(output_string,True,self._green)
+        text = self._small_font.render(output_string,True,self._green)
         self._screen.blit(text, [self.center(text), last_y])
 
     def draw_final(self, game):
-        pass
+        for i in range(5):
+            a,p = None, None
+            if not i in game.solved_fin_a.keys() or game.final.hide:
+                a = "."*self._alen
+                p = "--"
+            else:
+                a,p = game.final.questions[i][game.solved_fin_a[i]]
+            if i in game.final.solved_a.keys() and not i in game.solved_fin_a.keys():
+                game.solving_a = True
+                game.solving_num = i
+                game.solving_state = 1
+                game.solving_start = self._frame_count
+                game.solved_fin_a[i] = game.final.solved_a[i]
+                self._sound_answer.play()
+            if game.solving_a and game.solving_num == i:
+                a,p = self.solve_answer(i, game, a, p)
+            self.draw_answer(i, a, p) 
+        #self.draw_points(game)
         
-    def solve_answer(self, num, game):
-        a,p = game.get_round().answers[self._solving_num]
-        if self._solving_state == self._alen:
+    def solve_answer(self, num, game, a, p):
+        #a,p = game.get_round().answers[game.solving_num]
+        if game.solving_state == self._alen:
             self._sound_points.play()
-            self._solving = False
+            game.solving = False
+            game.solving_a = False
+            game.solving_b = False
         else:
             p = "--"
-        n = min(self._solving_state, len(a))
-        m = self._solving_state - n
+        n = min(game.solving_state, len(a))
+        m = game.solving_state - n
         s = "{}{}{}".format(a[:n], " "*m, "."*(self._alen - (n+m)))
-        self._solving_state += ((self._frame_count - self._solving_start) % 10) // 9
+        game.solving_state += ((self._frame_count - game.solving_start) % 5) // 4
         return s,p
 
 
@@ -232,33 +386,65 @@ class Renderer(object):
 
 
 class Session(object):
-    def __init__(self, q):
+    def __init__(self, q,r,x):
         self.game = None
         self._q = q
+        self._r = r
+        self._x = x
 
     def do(self,cmd):
-        c = cmd.split("#")
+        c = cmd.split("&")
         if c[0] == "load":
             self.game = Game()
-            self.game.set_team_a("foo")
-            self.game.set_team_b("bar", 23)
+            self.game.set_team_a("")
+            self.game.set_team_b("")
             self.game.load(c[1])
+            self._r.put("done")
         elif c[0] == "setround":
             self.game.set_round(int(c[1]))
-            self.game.rtype = "round"
+            self._r.put(self.game.get_round().get_data())
+        elif c[0] == "setfinal":
+            self.game.set_final()
+            self._r.put(self.game.final.get_data())
         elif c[0] == "solve":
             self.game.solve(int(c[1]))
+            self._r.put("0")
+        elif c[0] == "wrong":
+            self.game.wrong()
+            self._r.put("0")
+        elif c[0] == "quit":
+            self.running = False
+            self._r.put("0")
+            self._x.put("0")
+        elif c[0] == "setteama":
+            self.game.set_team_a(c[1])
+            self._r.put(c[1])
+        elif c[0] == "setteamb":
+            self.game.set_team_b(c[1])
+            self._r.put(c[1])
+        elif c[0] == "pointstb":
+            self.game.make_points(self.game.team_b)
+            self._r.put("0")
+        elif c[0] == "pointsta":
+            self.game.make_points(self.game.team_a)
+            self._r.put("0")
+        elif c[0] == "setpointsa":
+            self.game.team_a.points = int(c[1])
+            self._r.put("0")
+        elif c[0] == "setpointsb":
+            self.game.team_b.points = int(c[1])
+            self._r.put("0")
 
     def run(self):
-        running = False
+        self.running = False
         while self._q.empty():
             time.sleep(0.1)
-        if self._q.get() == "init":
-            running = True
+        if "init" in self._q.get():
+            self.running = True
         
         
         r = Renderer()
-        while not r.render(self.game) and running:
+        while not r.render(self.game) and self.running:
             if not self._q.empty():
                 self.do(self._q.get())
         
@@ -268,10 +454,11 @@ class Session(object):
 def main():
     q = Queue()
     r = Queue()
+    x = Queue()
 
-    recvr = Process(target=recv, args=(q,r))
+    recvr = Process(target=srvr, args=(q,r,x))
     recvr.start()
-    s = Session(q)
+    s = Session(q,r,x)
     s.run()
     r.put("stopit")
     recvr.join()
